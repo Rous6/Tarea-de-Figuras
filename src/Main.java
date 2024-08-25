e133cb3f44b9aaf738e2792655c2132e9789a55a
Rous <rousaronicjuro@gmail.com>
2024-08-25 16:39:36 -0500

    Tarea Actualizada 1

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import Clases.Triangulo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("te amo");
+        
         Figura Tri=new Triangulo(5,4);
         System.out.println("El area del triangulo es: "+Tri.Area());
         Figura Rec=new Rectangulo(5,6);
